9cfd120a225d73492fc72355bc35e6982662b58f
Olena Petryshak <petryshak94@gmail.com>
2019-09-13 13:52:36 +0300

    fitst commit

---
+++ target/classes/db/migration/V2__create_shipwreck.sql
@@ -0,0 +1,12 @@
+
+    CREATE TABLE SHIPWRECK(
+        ID INT AUTO_INCREMENT,
+        NAME VARCHAR(255),
+        DESCRIPTION varchar(2000),
+        CONDITION VARCHAR(255),
+        DEPTH INT,
+        LATITUDE DOUBLE,
+        LONGITUDE DOUBLE,
+        YEAR_DISCOVERED int
+    );
+
